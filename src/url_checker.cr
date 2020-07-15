2f9ac2c59af19f178d1d0402b306cf9cf5e38363
Danilo Barion Nogueira <danilob@knowbe4.com>
2020-07-15 16:33:08 -0300

    First implementation, calling URLs sync

--- src/url_checker.cr
+++ src/url_checker.cr
@@ -1,6 +1,24 @@
-# TODO: Write documentation for `UrlChecker`
-module UrlChecker
-  VERSION = "0.1.0"
+require "http/client"
+require "yaml"
 
-  # TODO: Put your code here
-end
+get_urls = ->{
+  File.open("./urls.yml") do |file|
+    YAML.parse(file)["urls"].as_a.map(&.as_s)
+  end
+}
+
+get_status = ->(url : String) {
+  begin
+    puts "calling #{url}"
+    response = HTTP::Client.get(url)
+    response.status_code
+  rescue e : Exception | Socket::ConnectError # Union types
+    {url, e}
+  ensure
+    puts "called #{url}"
+  end
+}
+
+puts get_urls.call.map(&get_status).join("\n")
+
+puts "Hello Crystal"
