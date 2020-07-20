572cf2dd65ceb79086fb7fa7736248517cd3849c
Danilo Barion Nogueira <danilob@knowbe4.com>
2020-07-20 16:31:21 -0300

    Rename channel

--- src/url_checker.cr
+++ src/url_checker.cr
@@ -4,13 +4,13 @@ require "./lib/tasks/stats_logger"
 require "./lib/tasks/printer"
 
 WORKERS = 2
-url_stream = Channel(String).new # Channel, with that type it receives
+url_result_stream = Channel(String).new # Channel, with the type it receives
 result_stream = Channel({String, Int32 | Exception}).new
 stats_stream = Channel(Array({String, Stats::UrlInfo})).new
 
-UrlReader.run("./urls.yml", url_stream)
+UrlReader.run("./urls.yml", url_result_stream)
 WORKERS.times {
-  StatusChecker.run(url_stream, result_stream)
+  StatusChecker.run(url_result_stream, result_stream)
 }
 StatsLogger.run(result_stream, stats_stream)
 Printer.run(stats_stream)
